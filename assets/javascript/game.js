9af8b6652ace19e6d31ec6d7a4a86a7a5322e4b5
Steve Yai <23424164+stephenyai@users.noreply.github.com>
2019-07-06 02:00:04 -0700

    still need to add css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,42 @@
 var foodWords = ['sushi', 'hamburger', 'pasta', 'pizza', 'apple', 'pineapple', 'galbi', 'omakase', 'bread', 'dumplings', 'gyro', 'taco', 'flauta', 'rice'];
 
-var maxTries = 10;
+var randomWord;
+var maxTries = 12;
 var numGuesses = 0;
 var totalWins = 0;
 var guessWord = [];
 var lettersGuessed = [];
+var startGame = false;
+var endGame = false;
 
 var wordString = document.querySelector('#wordPlace');
 var start = document.querySelector('.start');
 var winTotal = document.querySelector('#totalMeals');
 var guessesLeft = document.querySelector('#guessesLeft');
-var wrongLetters = document.querySelector('#lettersGuessed');
-var randomWord = foodWords[Math.floor(Math.random() * foodWords.length)];
-
-function guessCheck(letter) {
-
-	start.classList.add('hide');
-
-	if (event.keyCode >= 65 && event.keyCode <= 90) {
-		letter + 32;
-	} else {
-	}
-		
-	gameBegins()
-}
+var wrongLetters = document.querySelector('#lettersGuessed')
 
 function defaultPage() {
+
+	numGuesses = maxTries;
 	guessesLeft.innerText = maxTries;
 	winTotal.innerText = totalWins;
+	startGame = false;
+
+	randomWord = foodWords[Math.floor(Math.random() * foodWords.length)];
 
 	guessWord = [];
 	lettersGuessed = [];
 
 	for (var i=0; i<randomWord.length; i++) {
-		guessWord.push("_")
+		guessWord.push("_");
 	}
+
+	display();
 }
 
 function display() {
-	guessesLeft.innerText = maxTries-numGuesses;
+
+	guessesLeft.innerText = numGuesses;
 	winTotal.innerText = totalWins;
 	wrongLetters.innerText = lettersGuessed;
 	wordString.innerText = "";
@@ -46,30 +44,67 @@ function display() {
 	for (var i=0; i<guessWord.length; i++) {
 		wordString.innerText += guessWord[i];
 	}
+
+	if (numGuesses<=0) {
+		endGame = true;
+		wordString.innerText = "No food for you. Press any key to play again.";
+	}
+}
+
+function guessCheck() {
+
+	if (endGame) {	
+		defaultPage();
+		endGame = false;
+	} else {
+		if (event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode >= 97 && event.keyCode <= 122) {
+			noDuplicate();
+		} 
+	}
+}
+
+function noDuplicate() {
+
+	if (lettersGuessed.indexOf(event.key) === -1) {
+		start.classList.add('hide');
+
+		gameBegins();
+	}
+
+	display();
+	winCheck();
 }
 
 function gameBegins() {
 
-		var newArray = [];
-		var lowerCase = event.key.toLowerCase();
-		
-		for (var i=0; i<randomWord.length; i++) {
-			if (event.key === randomWord[i]) {
-				newArray.push(i);
-			}
-		}
+	var newArray = [];
+	var lowerCase = event.key.toLowerCase();
 
-		if (newArray.length<=0) {
-			numGuesses++;
-			lettersGuessed.push(event.key);
-		} else {
-			for (var i=0; i<newArray.length; i++) {
-				guessWord[newArray[i]] = event.key;
-				console.log('fii', guessWord);
-			}
+	for (var i=0; i<randomWord.length; i++) {
+		if (lowerCase === randomWord[i]) {
+			newArray.push(i);
 		}
-	
-		display();
+	}
+
+	if (newArray.length<=0) {
+		numGuesses--;
+		lettersGuessed.push(lowerCase);
+	} else {
+		for (var i=0; i<newArray.length; i++) {
+			guessWord[newArray[i]] = lowerCase;
+			console.log('fii', guessWord);
+		}
+	}
+
+	display();
+}
+
+function winCheck() {
+	if (guessWord.indexOf("_") === -1) {
+		wordString.innerText = "Today, you dine!";
+		totalWins++;
+		endGame = true;
+	}
 }
 
 document.onkeypress = guessCheck;
